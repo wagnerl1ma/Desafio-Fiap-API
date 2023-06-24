40d392ac9222a882f05b8055404015bfb4ef8e37
Wagner Lima <wagner.araujo92@outlook.com>
2023-06-24 15:17:08 -0300

    BaseService e notificadores

---
+++ src/CadastrosFiap.Business/Notificacoes/Notificacao.cs
@@ -0,0 +1,12 @@
+﻿namespace CadastrosFiap.Business.Notificacoes
+{
+    public class Notificacao
+    {
+        public string Mensagem { get; }
+
+        public Notificacao(string mensagem)
+        {
+            Mensagem = mensagem;
+        }
+    }
+}
